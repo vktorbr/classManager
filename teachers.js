93dc2f29630b0fb9398234894417db0d22169ed9
Victor Martins <vms5@cin.ufpe.br>
2021-10-08 19:40:35 -0300

    criacao da funcao de editar e deletar professores

--- teachers.js
+++ teachers.js
@@ -76,3 +76,54 @@ exports.edit = function(req, res){
 
     return res.render('teachers/edit', { teacher });
 }
+
+//é claro que um dia eu não vou conseguir
+//mas esse dia não é hoje
+//put
+exports.put = function(req, res){
+    const { id } = req.body;
+    let index = 0;
+
+    const foundTeacher = data.teachers.find(function(teacher, foundIndex){
+        if(id == teacher.id){
+            index = foundIndex;
+            return true;
+        }
+    })
+
+    if(!foundTeacher) return res.send('Teacher not found!');
+
+    const teacher = {
+        ...foundTeacher,
+        ...req.body,
+        birth: Date.parse(req.body.birth)
+    }
+
+    data.teachers[index] = teacher;
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err){
+        if(err) return res.send('Write file error!');
+    })
+
+    return res.redirect(`/teachers/${id}`);
+
+
+
+}
+
+//delete
+exports.delete = function(req, res){
+    const { id } = req.body;
+
+    const filteredTeachers = data.teachers.filter(function(teacher){
+        return id != teacher.id;
+    })
+
+    data.teachers = filteredTeachers;
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err){
+        if(err) return res.send('Write File error!');
+
+        return res.redirect('/teachers');
+    })
+}
--- utils.js
+++ utils.js
@@ -19,8 +19,8 @@ module.exports = {
         const birth = new Date(timestamp);
 
         const year = birth.getUTCFullYear();
-        const month = `0${birth.getUTCMonth() + 1}`.split(-2);
-        const day = `0${birth.getUTCDate()}`.split(-2);
+        const month = `0${birth.getUTCMonth() + 1}`.slice(-2);
+        const day = `0${birth.getUTCDate()}`.slice(-2);
 
         return `${year}-${month}-${day}`;
     },
